b65c335bf812acd9bff7ac8772614605a650774c
eaakashmaurya <eaakashmaurya@gmail.com>
2020-12-31 20:22:14 +0530

    Second commit

--- posts/models.py
+++ posts/models.py
@@ -9,38 +9,18 @@ from users.models import Profile
 class Post(models.Model):
     """Post Model."""
 
-    # user = models.ForeignKey(User, on_delete=models.CASCADE)
-    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    profile = models.ForeignKey(Profile, related_name="posts", null=True, on_delete=models.SET_NULL )
 
-    title = models.CharField(max_length=255)
-    photo = models.ImageField(upload_to='posts/photos')
-
+    title = models.CharField(max_length=255) #CAPTION
+    photo = models.ImageField(upload_to='posts/photos') 
+    
+    post_likes = models.ManyToManyField(Profile, default=False, blank=True, related_name="likes")
+    #charfield the tag
+    #tag_someone = models.ForeignKey(Profile, related_name="tag", blank=True, default=False)
+    
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         """Return title and username"""
         return "{} by @{}".format(self.title, self.profile.user.username)
-
-
-# class User(models.Model):
-#     """User model."""
-
-#     email = models.EmailField(unique=True)
-#     password = models.CharField(max_length=100)
-
-#     first_name = models.CharField(max_length=100)
-#     last_name = models.CharField(max_length=100)
-
-#     is_admin = models.BooleanField(default=False)
-
-#     bio = models.TextField(blank=True)
-
-#     birthdate = models.DateField(blank=True, null=True)
-
-#     created = models.DateTimeField(auto_now_add=True)
-#     modified = models.DateTimeField(auto_now=True)
-
-#     def __str__(self):
-#         """Return email."""
-#         return self.email
